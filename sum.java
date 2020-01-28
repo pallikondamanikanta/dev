41f3a20611726cd49e44682d0b7f7d3788adada1
pallikondamanikanta <manikanta.11195@gmail.com>
2020-01-28 11:38:52 +0530

    my 1st commit

---
+++ sum.java
@@ -0,0 +1,11 @@
+class sum {
+	public static void main (String[]args)
+	{
+		int a,b,c;
+		a=50;
+		b=30;
+		c=120;
+		c=a+b+c;
+		System.out.println("The sum of three valyes "+c);
+	}
+}       
---
+++ sum1.java
@@ -0,0 +1,8 @@
+
+
+
+
+
+
+jbbjkbkj
+bubk
